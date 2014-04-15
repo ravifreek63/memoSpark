a6b5e2bbeb219929d8a78068716acca52ce4095d
ravitandon <ravifreek63@gmail.com>
2014-04-15 19:24:36 -0400

    initialized self

--- core/src/main/scala/org/apache/spark/rdd/IndexedRDD.scala
+++ core/src/main/scala/org/apache/spark/rdd/IndexedRDD.scala
@@ -7,28 +7,27 @@ import collection.mutable.HashMap
 private[spark] class IndexedRDD[K: ClassTag](prev: RDD[K]) 
     extends RDD[K](prev) {
    
-  case class Pair(key:String, value: String)              
+  case class Pair(key:Any, value:Any)              
 
   object PairOrdering extends Ordering[Pair] {
-      def compare(a: Pair, b: Pair) = a.key compare b.key
+      def compare(a: Pair, b: Pair) = a.key.toString() compare b.key.toString()
     }
        
-  var self: RDD[String]
+  var self: RDD[(String, String)] = sortByKey()
   
-   def sortByKey():RDD[String] = {
-      var array = this.map {case (key, value) => new Pair(key.toString(), value.toString())}.collect() // Collecting the elements of this RDD into an array 
+   def sortByKey():RDD[(String, String)] = {
+      var array = this.map {case (key, value) => new Pair(key, value)}.collect() // Collecting the elements of this RDD into an array 
       Sorting.quickSort(array)(PairOrdering) // Sorting the elements of this array
-      self = this.context.parallelize(array).map(s => s.key) // Parallelize the given array and converts it into an RDD sorted by keys
-      self
+      this.context.parallelize(array).map(s => (s.key.toString(), s.value.toString())) // Parallelize the given array and converts it into an RDD sorted by keys
     }
    
    private var keyMap: HashMap[String, Int] = new HashMap[String, Int]() // Maps string to index 
    
    def buildIndex(): HashMap[String, Int] = {
-     val sortRDD: RDD[String] = sortByKey()
+     val sortRDD: RDD[(String, String)] = sortByKey()
      var index = 0
      sortRDD.foreach (s => {
-    	 keyMap(s) = index 
+    	 keyMap(s._1) = index 
     	 index += 1
        }
      )
