bfe62bbd626119be7b18d04ab05ddf4070d738c8
ravitandon <ravifreek63@gmail.com>
2014-04-14 23:31:50 -0400

    added incremental page rank

--- inc_page_rank/src/main/scala/IPageRank.scala
+++ inc_page_rank/src/main/scala/IPageRank.scala
@@ -4,10 +4,11 @@ import org.apache.spark.SparkContext._
 import org.apache.spark.SparkContext
 import scala.collection.mutable.HashSet
 import scala.collection.immutable.TreeMap
+import java.io._
 
 /**
- * Computes the PageRank of URLs from an input file. Input file should
- * be in format of:
+* Computes the PageRank of URLs from an input file. Input file should
+* be in format of:
  * URL         neighbor URL
  * URL         neighbor URL
  * URL         neighbor URL
@@ -24,7 +25,7 @@ object IPageRank {
   // function which filters the graph based on some change in the rank  
   def filterGraph(outP:TreeMap[String, Double], outPOld:TreeMap[String, Double]):HashSet[String] = {
       val jetSet = new HashSet[String]
-      val fChange = 0.1
+      val fChange = 0.01
       outP foreach ((element) => 
       if (outPOld.get(element._1) != None){
           for (ratio1_alias <- outPOld.get(element._1)){
@@ -37,7 +38,24 @@ object IPageRank {
       jetSet
     }
   
- def main(args: Array[String]) {
+   // Gets the rank of a key from the tree map  
+   def getRank (defaultValue: Double, key: String, oriRanks: scala.collection.immutable.TreeMap[String, Double]): Double = {
+       var rank:Double = defaultValue
+       if (oriRanks.get(key) != None){
+        for (ratio1_alias <- oriRanks.get(key)){
+         rank = ratio1_alias
+        } 
+       }
+       rank
+    }
+  
+  def writeRanks(fileName: String, output:Array[(String, Double)]) = {
+       val writer = new PrintWriter(new File(fileName)) 
+       output.foreach(tup => writer.write(tup._1 + "\t" + (tup._2).toString + "\n")) 
+       writer.close()
+  }
+    
+  def main(args: Array[String]) {
    if (args.length < 3) {
      System.err.println("Usage: PageRank <master> <file> <number_of_iterations>")
      System.exit(1)
@@ -48,13 +66,24 @@ object IPageRank {
     System.setProperty("spark.executor.memory", "1024m")
   val ctx = new SparkContext(args(0), "SparkPageRank",
       "/memex/tandon/memoSpark", List("/memex/tandon/memoSpark/inc_page_rank/target/scala-2.10/page-rank_2.10-1.0.jar"))
+    // Reading the file in which ranks are stored
+    var lines2 = ctx.textFile(args(4), 1)
+    // Reading the ranks into an RDD
+    /*val oRanks = scala.collection.immutable.TreeMap(lines2.map{s => 
+      val parts = s.split("\\s+")
+      (parts(0), parts(1).toDouble)
+    }.collect():_*)*/ 
     val lines = ctx.textFile(args(1), 1)
     val startTime = System.nanoTime
+    // Getting the set of new links 
     var links = lines.map{ s =>
       val parts = s.split("\\s+")
       (parts(0), parts(1))
     }.distinct().groupByKey().cache()
-    var ranks = links.mapValues(v => 1.0)
+ 
+    // mapping the ranks needs to change too 
+    var ranks = links.mapValues(v => 1.0)  
+    var updatedRanks = Map(ranks.collect():_*)
     val iters = args(2).toInt
     var outP = new TreeMap[String, Double]()
     var outPOld = new TreeMap[String, Double]()
@@ -67,13 +96,14 @@ object IPageRank {
       }
       
       ranks = contribs.reduceByKey(_ + _).mapValues(0.15 + 0.85 * _)
-      println("iter:"+i+",ranks-length:"+ranks.count())
+      
          if (i == 1)
            outP = scala.collection.immutable.TreeMap(ranks.collect():_*)
-         if(i%10 == 1)
+         if(i%5 == 1)
           outPOld = outP
-         if(i%10  == 0) { 
+         if(i%5  == 0) { 
           outP = scala.collection.immutable.TreeMap(ranks.collect():_*)
+          updatedRanks = updatedRanks.map {case(key,value) => (key, getRank(value, key, outP))}
           filterNodes = filterGraph (outP, outPOld)
           ranks = ranks.filter(node => filterNodes.contains(node._1))
        }
@@ -82,6 +112,8 @@ object IPageRank {
     val endTime = System.nanoTime
     val totalTime = (endTime - startTime)/1e6
     println("totalTime:" + totalTime + ":links_count:" + lines.count())
+    writeRanks(args(5), updatedRanks.toArray)
+//  updatedRanks.foreach(tup => println(tup._1 + " has rank: " + tup._2 + "."))
 //  output.foreach(tup => println(tup._1 + " has rank: " + tup._2 + "."))
     System.exit(0)
   }
